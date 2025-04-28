8dee5a95bff17c06e39a0b310a87d17d1340ef14
adriano barbosa <adrianobarbosa3001@gmail.com>
2025-04-28 17:04:27 -0300

    First commit

---
+++ src/main/java/com/api/TCCnnect/TcCnnectApplication.java
@@ -0,0 +1,13 @@
+package com.api.TCCnnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcCnnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcCnnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/TCCnnect/TcCnnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.TCCnnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcCnnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
