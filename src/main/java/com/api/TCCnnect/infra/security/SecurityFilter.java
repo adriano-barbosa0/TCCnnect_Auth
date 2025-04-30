02a25a22e1c0eee9bc11c557fa3d4ded6a4eea15
Douglas Porto <portogdouglas@gmail.com>
2025-04-30 16:51:17 -0300

    feat/infra: Implementação do sistema de autenticação com JWT

--- src/main/java/com/api/TCCnnect/infra/security/SecurityFilter.java
+++ src/main/java/com/api/TCCnnect/infra/security/SecurityFilter.java
@@ -2,6 +2,7 @@ package com.api.TCCnnect.infra.security;
 
 import com.api.TCCnnect.repository.UsuarioRepository;
 import com.api.TCCnnect.services.TokenService;
+import com.api.TCCnnect.services.impl.TokenServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,14 +16,15 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
-    private final TokenService tokenService;
+    private final TokenServiceImpl tokenService;
     private final UsuarioRepository usuarioRepository;
 
-    public SecurityFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
+    public SecurityFilter(TokenServiceImpl tokenService, UsuarioRepository usuarioRepository) {
         this.tokenService = tokenService;
         this.usuarioRepository = usuarioRepository;
     }
 
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
         String tokenJWT = getToken(request);
--- src/main/java/com/api/TCCnnect/model/Usuario.java
+++ src/main/java/com/api/TCCnnect/model/Usuario.java
@@ -1,25 +1,63 @@
 package com.api.TCCnnect.model;
 
 import com.api.TCCnnect.dto.enums.UserRole;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Data
+import java.util.Collection;
+import java.util.List;
+
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario {
+@Table(name = "usuarios")
+@Getter
+@EqualsAndHashCode(of = "id")
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Column(nullable = false, unique = true)
     private String login;
+
+    @Column(nullable = false)
     private String password;
-    private UserRole role;
+
+//    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public String getUsername() {
+        return login;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 
 }
