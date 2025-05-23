52e7a45a5b14488e2408c96e546d81aa1a0b912c
Douglas Porto <portogdouglas@gmail.com>
2025-05-23 15:10:51 -0300

    feat: Adicionar configuração de esquemas DTOs para documentação com Swagger

--- src/main/java/com/api/TCCnnect/dto/UserProfileDTO.java
+++ src/main/java/com/api/TCCnnect/dto/UserProfileDTO.java
@@ -2,7 +2,7 @@ package com.api.TCCnnect.dto;
 
 import java.util.UUID;
 
-public record UserProfileDTO(UUID id, String login, String name, String bio, String avatarUrl) {
+public record UserProfileDTO(UUID id, String login, String name, String bio, String avatar_url) {
 
 
 }
